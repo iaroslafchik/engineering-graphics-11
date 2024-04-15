828293a3174376ad1351716ed1225f8d8325bd58
Yorick <y.kovanev@mail.ru>
2024-04-15 09:18:28 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphics_lab_1_1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Input filename");
+      string filename = Console.ReadLine();
+      string[] text = System.IO.File.ReadAllLines(filename);
+      for (int i = 0 ; i < text.Length; i++) {
+        Console.WriteLine(text[i]);
+      }
+    }
+  }
+}
